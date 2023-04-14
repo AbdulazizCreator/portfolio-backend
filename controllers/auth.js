29e08410e5504ca0ab8216e2088f518053d1fa11
abdulazizcreator <abdulazizprogrammer@gmail.com>
2023-04-14 17:54:16 +0500

    user

--- controllers/auth.js
+++ controllers/auth.js
@@ -160,7 +160,7 @@ exports.deleteUserImage = asyncHandler(async (req, res, next) => {
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
-
+  console.log(user);
   const options = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
@@ -170,8 +170,9 @@ const sendTokenResponse = (user, statusCode, res) => {
   if (process.env.NODE_ENV === "production") {
     options.secure = true;
   }
+  delete user.password;
   res
     .status(statusCode)
     .cookie("token", token, options)
-    .json({ success: true, token });
+    .json({ success: true, token, user });
 };
